cf4c294a1432b54b20bd3baacb82d6d761838165
“hejiawei” <469994761@qq.com>
2023-03-01 14:05:27 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入网络请求的包 $http 对象
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求拦截器 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 响应拦截器 请求完成之后做一些事情
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none',
+	})
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
